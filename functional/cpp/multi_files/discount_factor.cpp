97f75b37bad8cf1e00428266c6f3c3d6ab3cba58
SarahAmrane0 <sarah.amrane@dauphine.eu>
2018-10-25 15:43:55 +0200

    functinoal session

--- functional/cpp/multi_files/discount_factor.cpp
+++ functional/cpp/multi_files/discount_factor.cpp
@@ -1,1 +1,22 @@
 // Put here the functions discount_factor and print_discount_factor
+
+
+#include <cmath>
+#include <iostream>
+#include "discount_factor.hpp"
+
+/* double discount_factor(double rate, double maturity)
+    {
+        double res = std::exp(-rate * maturity);
+        return res;
+    }
+*/
+    void print_discount_factor(double rate, double maturity)
+    {
+        double df = discount_factor(rate, maturity);
+        std::cout << "DF(" << maturity << "," << rate << ") = " << df << std::endl;
+        // No return statement here
+
+
+    }
+static int check = 0;
--- functional/cpp/multi_files/discount_factor.hpp
+++ functional/cpp/multi_files/discount_factor.hpp
@@ -1,1 +1,19 @@
 // Put here the declarations of discount_factor and print_discount_factor
+
+#ifndef DISCOUNT_FACTOR_HPP // Inclusion Guard
+#define DISCOUNT_FACTOR_HPP
+#include <iostream>
+#include <cmath>
+#include <vector>
+// discount_factor.hpp
+    double discount_factor(double rate, double maturity);
+    std::vector<double> discount_factor(const std::vector<double> r, const std::vector<double> m);
+    void print_discount_factor(double rate, double maturity);
+
+
+inline double discount_factor(double rate, double maturity)
+    {
+        double res = std::exp(-rate * maturity);
+        return res;
+    }
+#endif // DISCOUNT_FACTOR_HPP
